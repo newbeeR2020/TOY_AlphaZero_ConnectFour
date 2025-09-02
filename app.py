6e5bfcea09fc469bee625735ee7e272a74469832
Rick Abiko <64783615+newbeeR2020@users.noreply.github.com>
2025-09-01 21:37:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -292,7 +292,7 @@ st.title("🤖 コネクトフォーAI対戦 🔴🟡")
 
 # モデルとMCTSのインスタンスを準備
 net = load_ai_model()
-mcts = MCTS(net, sims=1000)
+mcts = MCTS(net, sims=1600)
 
 # ゲームが開始されたかどうかの状態を管理
 if 'game_started' not in st.session_state:
